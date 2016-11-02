c5b4793e1c5348d30bc6abafb5f0280d0adc6bf6
Soledad Coronel <soledad.coronel@gointegro.com>
2016-11-02 14:32:03 -0300

    .

--- src/fixtures/platform.js
+++ src/fixtures/platform.js
@@ -23,26 +23,26 @@ class PlatformFixture {
 				"data": 
 				[
 				{
-						"type": "platforms",
-						"attributes": 
-						{
+					"type": "platforms",
+					"attributes": 
+					{
 					"name": "miPrimerTest"+random.integer(1, 10000),
 					"subdomain": "miPrimerTest"+random.integer(1, 10000),
 					"timezone": "America/Argentina/Buenos_Aires",
 					"status": "active",
 					"users-range": "0-50",
 					"language": "es"
-						}
+					}
 				},
 				{
-						"type": "users",
-						"attributes": 
-						{
-						"name": "Soledad",
-						"last-name": "Coronel",
-						"email": "soledad.coronel@gointegro.com",
-						"password": "coquito25"
-						}
+					"type": "users",
+					"attributes": 
+					{
+					"name": "Soledad",
+					"last-name": "Coronel",
+					"email": "soledad.coronel@gointegro.com",
+					"password": "coquito25"
+					}
 				}
 				]
 		}
