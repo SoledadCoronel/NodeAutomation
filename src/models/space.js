f71df22e3dc8195959e21522dfcc6674045128e5
Soledad Coronel <soledad.coronel@gointegro.com>
2016-12-26 12:17:11 -0300

    se comenta api.js hasta terminar el refactor

--- src/models/space.js
+++ src/models/space.js
@@ -1,10 +1,14 @@
-/*import AbstractModel from './abstractModel';
 
+/*import {Serializer, Deserializer} from 'jsonapi-serializer';
+import AbstractModel from './abstractModel';
 
 class Space extends AbstractModel {
 
 	constructor (data{}) {
+		super();
+		this.serializer = new Serializer;
 
+		this.id = data.id; 
 
 
 	}
