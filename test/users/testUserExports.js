f34a47b3282e657f969dbc2eecf6f3ef87dfc68e
Soledad Coronel <soledad.coronel@gointegro.com>
2017-09-26 14:02:16 -0300

    se agregan tests para descarga de template y descarga de usuarios

--- test/users/testUserExports.js
+++ test/users/testUserExports.js
@@ -16,31 +16,31 @@ chai.use(chaiColors);
 
 describe('SUITE - USERS - USER EXPORTS', function() {
 
-/*it('case 1: ', function(done) {
-	session.addToken(1, jsonData.adminToken);
-
-
-	new UserExports()
-	.list({filter: {'template': 1}})
-	.then((response) => {
-		console.log(JSON.stringify(response.content, null, 2));
-		//response.should.have.status('200');
-		//response.content.elements.should.be.a('array');
-		//response.content.elements.length.should.be.eql(2);
-		//expect(response.content.meta.pagination['total-items']).to.equal(2);
-        done();
-    });
-});*/
-
-it('Caso 16: gets template', function(done) {
+it('Caso 1: Download user template', function(done) {
 	chai.request('http://api.cd.gointegro.net')
-	.get('/user-exports/')
+	.get('/user-exports/' + '?' + 'filter[template]=1')
 	.set('content-type', 'application/vnd.api+json')
 	.set('Accept', 'application/vnd.api+json')
 	.set('Authorization', 'Bearer ' + jsonData.adminToken)
 	.end(function(err, res) {
-		console.log(res)
-		//res.should.have.status(204);
+		res.should.have.status(200);
+		expect(res.text).to.equal('first_name,last_name,status,access,role,email,supervisor_email,job_title,groups,employee_id,country,job_phone,extension,job_cellphone,personal_phone,personal_cellphone,linkedin,facebook,twitter,admission_date,job_address,personal_address,document_type,document,gender,birthdate,personal_email,marital_status,job_type\n');
+		done();
+	});
+});
+
+it('Caso 2: Download platform users', function(done) {
+
+	chai.request('http://api.cd.gointegro.net')
+	.get('/user-exports/' + '?' + 'filter[template]=0')
+	.set('content-type', 'application/vnd.api+json')
+	.set('Accept', 'application/vnd.api+json')
+	.set('Authorization', 'Bearer ' + jsonData.adminToken)
+	.end(function(err, res) {
+		res.should.have.status(200);
+		assert.include(res.text, 'Soledad', 'string contains substring');
+		assert.include(res.text, 'UsuarioRolBasico', 'string contains substring');
+		assert.include(res.text, 'UsuarioRolAdminDeEspacio', 'string contains substring');
 		done();
 	});
 });
