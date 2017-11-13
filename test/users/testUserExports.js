b430097f08dcca21a241e1777a0493e0326a1279
Soledad Coronel <soledad.coronel@gointegro.com>
2017-11-13 14:00:29 -0300

    user-exports con payload por id y filters

--- test/users/testUserExports.js
+++ test/users/testUserExports.js
@@ -36,24 +36,54 @@ it('Caso 2: Download platform users', function(done) {
 	.send('access_token=' + jsonData.adminToken)
 	.end(function(err, res) {
 		res.should.have.status(200);
-		console.log(res.text);
 		assert.include(res.text, 'Soledad', 'string contains substring');
 		assert.include(res.text, 'UsuarioRolAdminDeEspacio', 'string contains substring');
 		done();
 	});
 });
 
-it('Caso 3: Download platform users by ids', function(done) {
+it('Caso 3: Download platform users by id', function(done) {
 
 	chai.request('http://api.cd.gointegro.net')
 	.post('/user-exports/')
 	.type('form')
 	.send('access_token=' + jsonData.adminToken)
-	.send('payload:{"filters": {"q":"Soledad"}}')
-	//.send(`payload={"ids":"${jsonData.basicUser.id},${jsonData.adminSpaceUser.id}"}`)
+	//.send(`payload={"filters": {"q":"UsuarioRolAdminDeEspacio"}}`)
+	.send(`payload={"ids":"${jsonData.adminUserId}"}`)
 	.end(function(err, res) {
-		console.log(res.text);
-		//res.should.have.status(200);
+		assert.include(res.text, 'Soledad', 'string contains substring');
+		res.should.have.status(200);
+		done();
+	});
+});
+
+it('Caso 4: Download platform users by ids', function(done) {
+
+	chai.request('http://api.cd.gointegro.net')
+	.post('/user-exports/')
+	.type('form')
+	.send('access_token=' + jsonData.adminToken)
+	//.send(`payload={"filters": "{"q":"UsuarioRolAdminDeEspacio"}""}`)
+	.send(`payload={"ids":"${jsonData.adminUserId},${jsonData.adminSpaceUser.id}"}`)
+	.end(function(err, res) {
+		assert.include(res.text, 'Soledad', 'string contains substring');
+		assert.include(res.text, 'UsuarioRolAdminDeEspacio', 'string contains substring');
+		res.should.have.status(200);
+		done();
+	});
+});
+
+it('Caso 5: Download platform users by filters q', function(done) {
+
+	chai.request('http://api.cd.gointegro.net')
+	.post('/user-exports/')
+	.type('form')
+	.send('access_token=' + jsonData.adminToken)
+	.send(`payload={"filters": {"q":"Soledad"}}`)
+	.end(function(err, res) {
+		assert.include(res.text, 'Soledad', 'string contains substring');
+		assert.include(res.text, 'active', 'string contains substring');
+		res.should.have.status(200);
 		done();
 	});
 });
